8b1c8605436aec16daafe7106633303241b5cf3f
esiammd <esiammd@hotmail.com>
2024-01-16 17:53:04 +0100

    feat: create Success page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Success from '../pages/Success';
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/success" element={<Success />} />
   </Routes>
 );
 
