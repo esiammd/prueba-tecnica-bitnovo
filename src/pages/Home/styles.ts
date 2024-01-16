c5abf4d25eabf84fa87b50f3dfcad7aaba03716a
esiammd <esiammd@hotmail.com>
2024-01-16 12:52:13 +0100

    first commit

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  size: 30px;
+  text-align: center;
+  color: #002859;
+`;
