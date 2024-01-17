626dd2302f78a8ea768f6af06d7d4a063c53e53f
esiammd <esiammd@hotmail.com>
2024-01-17 11:17:50 +0100

    feat: create Checkout page

---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: start;
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Checkout from '../pages/Checkout';
 import Success from '../pages/Success';
 import Canceled from '../pages/Canceled';
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/checkout" element={<Checkout />} />
     <Route path="/success" element={<Success />} />
     <Route path="/canceled" element={<Canceled />} />
   </Routes>
