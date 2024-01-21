7a5ac5f87c2eb8dac23032dca1ea2ba437f9d016
esiammd <esiammd@hotmail.com>
2024-01-21 16:47:27 +0100

    fix: update src image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ import { Container } from '../styles/components/Footer';
 const Footer: React.FC = () => {
   return (
     <Container>
-      <Image src="logo.svg" alt="Bitnovo" width={164} height={26} />
+      <Image src="/logo.svg" alt="Bitnovo" width={164} height={26} />
       <span>© 2022 Bitnovo. All rights reserved.</span>
     </Container>
   );
